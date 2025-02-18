53209f28815e66d51734e86bfcd10dc7af41c05e
MAHESHWARI A Uma <uma.maheshwari@soprasteria.com>
2025-02-18 17:16:20 +0530

    support

--- src/Components/Support/Support.jsx
+++ src/Components/Support/Support.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 
+import gridimages from '../../asset/imagesgrid.png'
+
 const Support = () => {
   return (
-    <div>
-      Support
+    <div className='support container section'>
+      <div className='sectionContainer'>
+
+        <div className='titlesDiv'>
+          <small>travel support</small>
+          <h2>Plan your travel with confidence</h2>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id sint tempore sunt porro beatae ratione. Blanditiis harum, vitae, beatae sed, illo error est enim id eligendi quibusdam perferendis. Blanditiis, cupiditate!</p>
+        </div>
+
+        <div className='infoDiv grid'>
+          <div className='textDiv grid'>
+
+            <div className='singleInfo'>
+              <span className='number'>01</span>
+              <h4>Travel requirements for Toronto</h4>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum deleniti nesciunt, nemo, molestias dicta incidunt, a alias quibusdam magnam aliquid error iusto. Optio nihil voluptatem saepe dolor illum adipisci earum.</p>
+            </div>
+
+            <div className='singleInfo'>
+              <span className='number colorOne'>02</span>
+              <h4>Travel requirements for Toronto</h4>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum deleniti nesciunt, nemo, molestias dicta incidunt, a alias quibusdam magnam aliquid error iusto. Optio nihil voluptatem saepe dolor illum adipisci earum.</p>
+            </div>
+
+            <div className='singleInfo'>
+              <span className='number colorTwo'>03</span>
+              <h4>Travel requirements for Toronto</h4>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum deleniti nesciunt, nemo, molestias dicta incidunt, a alias quibusdam magnam aliquid error iusto. Optio nihil voluptatem saepe dolor illum adipisci earum.</p>
+            </div>
+
+          </div>
+
+          <div>
+            <div className='imgDiv'>
+              <img src={gridimages} alt=''/>
+            </div>
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
